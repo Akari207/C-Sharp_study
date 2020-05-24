286eaa4b83a77a147dc6b8b7583b0c5c6db3786d
Akari207 <akari.kanbe@unique-log.com>
2020-05-25 03:27:44 +0900

    init file

---
+++ README.md
@@ -0,0 +1,11 @@
+## dotnet コマンド
+
+### dotnet new <template>
+* console `コンソール アプリケーション`
+
+
+
+## 実行の仕方
+(例)コンソールアプリケーションの場合。
+* dotnet new console
+* dotnet run
---
+++ example.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace C__study
+{
+   class Sample
+   {
+      static void Main() 
+      {
+         Console.WriteLine("C# gives you programming power");
+      }
+   }
+}
